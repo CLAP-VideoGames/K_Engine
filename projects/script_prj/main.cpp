ead1211adcfc7df97c5622d5c4c8e14582b048d7
Ángel López <angelo06@ucm.es>
2022-03-01 11:47:47 +0100

    Ejemplo de Lua con errores

--- projects/script_prj/main.cpp
+++ projects/script_prj/main.cpp
@@ -1,31 +1,22 @@
 // C++ program to illustrate the use of
 // LuaCpp library
-#include <LuaCpp.hpp>
-#include <iostream>
-using namespace LuaCpp::Registry;
-using namespace std;
-  
-// Driver Code
-int main(int argc, char** argv)
-{
-  
-    cout << "Hi from C++, this is a demo"
-         << " how LuaCpp can be used\n";
-  
-    LuaContext lua;
-  
-    // The simples way is to use
-    // CompileStringAndRun method
-    try {
-  
-        lua.CompileStringAndRun(
-            "print('The fastest way to "
-            "start using lua in "
-            "a project')");
-    }
-  
-    catch (std::runtime_error& e) {
-        std::cout << e.what()
-                  << '\n';
-    }
+#include <stdio.h>
+#include <lua.h>
+#include <LuaBridge.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+using namespace luabridge;
+
+int main() {
+    lua_State* L = luaL_newstate();
+    luaL_dofile(L, "script.lua");
+    luaL_openlibs(L);
+    lua_pcall(L, 0, 0, 0);
+    LuaRef s = getGlobal(L, "testString");
+    LuaRef n = getGlobal(L, "number");
+    std::string luaString = s.cast<std::string>();
+    int answer = n.cast<int>();
+    std::cout << luaString << std::endl;
+    std::cout << "And here's our number:" << answer << std::endl;
 }
