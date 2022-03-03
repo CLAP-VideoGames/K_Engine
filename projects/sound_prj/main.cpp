c0f639f7966fd58e46c31dcbd6fecc59c0df20a5
David Brey <dbrey@ucm.es>
2022-03-03 19:19:19 +0100

    Configurando SDL_Mixer

--- projects/sound_prj/main.cpp
+++ projects/sound_prj/main.cpp
@@ -28,31 +28,25 @@
  *
  */
 
-#include <stdio.h>
-#include <stdbool.h>
+//#include <stdbool.h>
 #include <iostream>
 
-#include <SDL.h>
+#include <SDL.h> // Esto da error
 #include <SDL_mixer.h>
 
- // Define MAX and MIN macros
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+//#define WAVES_SOUND "assets/clap.wav"
 
-// Define screen dimensions
-#define SCREEN_WIDTH    800
-#define SCREEN_HEIGHT   600
-
-#define WAVES_SOUND "assets/waves-at-baltic-sea-shore/waves-at-baltic-sea-shore.wav"
-
-#define CLAP_SOUND              "assets/claps-and-snares/clap.ogg"
-#define SNARE_SOUND             "assets/claps-and-snares/snare.ogg"
-#define TECHNO_CLAP_SNARE_SOUND "assets/claps-and-snares/techno-clap-snare.ogg"
-#define REVERB_SNARE_SOUND      "assets/claps-and-snares/dubstep-reverb-snare.ogg"
+//#define CLAP_SOUND             
+//#define SNARE_SOUND             "assets/snare.mp3"
+//#define TECHNO_CLAP_SNARE_SOUND "assets/claps-and-snares/snare.mp3"
+//#define REVERB_SNARE_SOUND      "assets/claps-and-snares/dubstep-reverb-snare.ogg"
 
 
-int main()
+
+int main(int argc, char* argv[])
 {
+	const char* SNARE_SOUND = "assets/snare.mp3";
+	const char* WAVES_SOUND = "assets/clap.wav";
 
 	// Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
@@ -60,6 +54,7 @@ int main()
 		/*printf("SDL could not be initialized!\n"
 			"SDL_Error: %s\n", SDL_GetError());
 		return 0;*/
+
 	}
 
 	//Initialize SDL2_mixer
@@ -68,195 +63,111 @@ int main()
 		/*printf("SDL2_mixer could not be initialized!\n"
 			"SDL_Error: %s\n", SDL_GetError());
 		return 0;*/
+		std::cout << "SDL2_mixer could not be initialized \n";
 	}
 
-#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
-	// Disable compositor bypass
-	if (!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
+	// Load .WAV sound
+	Mix_Chunk* waves = Mix_LoadWAV(WAVES_SOUND);
+	if (!waves)
 	{
-		printf("SDL can not disable compositor bypass!\n");
+		/*printf(".WAV sound '%s' could not be loaded!\n"
+			"SDL_Error: %s\n", WAVES_SOUND, SDL_GetError());*/
+		std::cout << ".wav sound could not be loaded \n";
 		return 0;
 	}
-#endif
 
-	// Create window
-	SDL_Window* window = SDL_CreateWindow("SDL2 audio sample (Press SPACE to pause/play)",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH, SCREEN_HEIGHT,
-		SDL_WINDOW_SHOWN);
-	if (!window)
+	// Load .OGG sounds
+	//Mix_Music* clapnsnare1 = Mix_LoadMUS(CLAP_SOUND);
+	Mix_Music* clapnsnare2 = Mix_LoadMUS(SNARE_SOUND);
+	//Mix_Music* clapnsnare3 = Mix_LoadMUS(TECHNO_CLAP_SNARE_SOUND);
+	//Mix_Music* clapnsnare4 = Mix_LoadMUS(REVERB_SNARE_SOUND);
+	if (!clapnsnare2)
 	{
-		/*printf("Window could not be created!\n"
-			"SDL_Error: %s\n", SDL_GetError());*/
+		std::cout << "One of the .mp3 sounds could not be loaded!\n SDL_Error: ";
+		std::cout << SDL_GetError();
+		return 0;
 	}
-	else
+
+	// Play waves sound
+	if (Mix_PlayChannel(-1, waves, 0) == -1)
 	{
-		// Create renderer
-		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		if (!renderer)
+		std::cout << "Waves sound could not be played!\n SDL_Error: ";
+		std::cout << SDL_GetError();
+
+		Mix_FreeChunk(waves);
+		return 0;
+	}
+
+	// Event loop exit flag
+	bool quit = false;
+
+	// Event loop
+	while (!quit)
+	{
+		SDL_Event e;
+
+		// Wait indefinitely for the next available event
+		SDL_WaitEvent(&e);
+
+		// User requests quit
+		if (e.type == SDL_QUIT)
 		{
-			/*printf("Renderer could not be created!\n"
-				"SDL_Error: %s\n", SDL_GetError());*/
+			quit = true;
 		}
-		else
+		else if (e.type == SDL_KEYDOWN)
 		{
-			// Load .WAV sound
-			Mix_Chunk* waves = Mix_LoadWAV(WAVES_SOUND);
-			if (!waves)
+			switch (e.key.keysym.sym)
 			{
-				/*printf(".WAV sound '%s' could not be loaded!\n"
-					"SDL_Error: %s\n", WAVES_SOUND, SDL_GetError());*/
-				return 0;
-			}
-
-			// Load .OGG sounds
-			Mix_Music* clapnsnare1 = Mix_LoadMUS(CLAP_SOUND);
-			Mix_Music* clapnsnare2 = Mix_LoadMUS(SNARE_SOUND);
-			Mix_Music* clapnsnare3 = Mix_LoadMUS(TECHNO_CLAP_SNARE_SOUND);
-			Mix_Music* clapnsnare4 = Mix_LoadMUS(REVERB_SNARE_SOUND);
-			if (!clapnsnare1 || !clapnsnare2 || !clapnsnare3 || !clapnsnare4)
-			{
-				std::cout << "One of the .OGG sounds could not be loaded!\n SDL_Error: ";
-				std::cout << SDL_GetError();
-				return 0;
-			}
-
-			// Play waves sound
-			if (Mix_PlayChannel(-1, waves, 0) == -1)
-			{
-				std::cout << "Waves sound could not be played!\n SDL_Error: ";
-				std::cout << SDL_GetError();
-			
-				Mix_FreeChunk(waves);
-				return 0;
-			}
-
-			// Declare rect of square
-			SDL_Rect squareRect;
-
-			// Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-			squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-			squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-			// Square position: In the middle of the screen
-			squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-			squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-			// Declare rects of pause symbol
-			SDL_Rect pauseRect1, pauseRect2;
-			pauseRect1.h = squareRect.h / 2;
-			pauseRect1.w = 40;
-			pauseRect1.x = squareRect.x + (squareRect.w - pauseRect1.w * 3) / 2;
-			pauseRect1.y = squareRect.y + squareRect.h / 4;
-			pauseRect2 = pauseRect1;
-			pauseRect2.x += pauseRect1.w * 2;
-
-
-			// Event loop exit flag
-			bool quit = false;
-
-			// Event loop
-			while (!quit)
-			{
-				SDL_Event e;
-
-				// Wait indefinitely for the next available event
-				SDL_WaitEvent(&e);
-
-				// User requests quit
-				if (e.type == SDL_QUIT)
-				{
-					quit = true;
-				}
-				else if (e.type == SDL_KEYDOWN)
-				{
-					switch (e.key.keysym.sym)
-					{
-					case SDLK_SPACE:
-						if (Mix_Paused(-1))
-							Mix_Resume(-1);
-						else
-							Mix_Pause(-1);
-						break;
-
-					case SDLK_RIGHT:
-						if (Mix_PlayMusic(clapnsnare1, 1) == -1)
-						{
-							printf(".OGG sound could not be played!\n"
-								"SDL_Error: %s\n", SDL_GetError());
-						}
-						break;
-
-					case SDLK_LEFT:
-						if (Mix_PlayMusic(clapnsnare2, 1) == -1)
-						{
-							printf(".OGG sound could not be played!\n"
-								"SDL_Error: %s\n", SDL_GetError());
-						}
-						break;
-
-					case SDLK_UP:
-						if (Mix_PlayMusic(clapnsnare3, 1) == -1)
-						{
-							printf(".OGG sound could not be played!\n"
-								"SDL_Error: %s\n", SDL_GetError());
-						}
-						break;
-
-					case SDLK_DOWN:
-						if (Mix_PlayMusic(clapnsnare4, 1) == -1)
-						{
-							printf(".OGG sound could not be played!\n"
-								"SDL_Error: %s\n", SDL_GetError());
-						}
-						break;
-					}
-				}
-
-				// Initialize renderer color white for the background
-				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-				// Clear screen
-				SDL_RenderClear(renderer);
-
-				// Set renderer color blue to draw the square
-				SDL_SetRenderDrawColor(renderer, 0x19, 0x71, 0xA9, 0xFF);
-
-				// Draw filled square
-				SDL_RenderFillRect(renderer, &squareRect);
-
-				// Check pause status
+			case SDLK_SPACE:
 				if (Mix_Paused(-1))
+					Mix_Resume(-1);
+				else
+					Mix_Pause(-1);
+				break;
+
+			case SDLK_RIGHT:
+				/*if (Mix_PlayMusic(clapnsnare1, 1) == -1)
 				{
-					// Set renderer color white to draw the pause symbol
-					SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					printf(".OGG sound could not be played!\n"
+						"SDL_Error: %s\n", SDL_GetError());
+				}*/
+				break;
 
-					// Draw pause symbol
-					SDL_RenderFillRect(renderer, &pauseRect1);
-					SDL_RenderFillRect(renderer, &pauseRect2);
+			case SDLK_LEFT:
+				if (Mix_PlayMusic(clapnsnare2, 1) == -1)
+				{
+					/*printf(".OGG sound could not be played!\n"
+						"SDL_Error: %s\n", SDL_GetError());*/
+					std::cout << (".mp3 sound could not be played\n"
+						"SDL_Error: %s\n", SDL_GetError());
 				}
+				break;
 
+			case SDLK_UP:
+				/*if (Mix_PlayMusic(clapnsnare3, 1) == -1)
+				{
+					printf(".OGG sound could not be played!\n"
+						"SDL_Error: %s\n", SDL_GetError());
+				}*/
+				break;
 
-				// Update screen
-				SDL_RenderPresent(renderer);
+			case SDLK_DOWN:
+				/*if (Mix_PlayMusic(clapnsnare4, 1) == -1)
+				{
+					printf(".OGG sound could not be played!\n"
+						"SDL_Error: %s\n", SDL_GetError());
+				}*/
+				break;
 			}
-
-			// Clean up audio
-			Mix_FreeChunk(waves);
-			Mix_FreeMusic(clapnsnare1);
-			Mix_FreeMusic(clapnsnare2);
-			Mix_FreeMusic(clapnsnare3);
-			Mix_FreeMusic(clapnsnare4);
-
-			// Destroy renderer
-			SDL_DestroyRenderer(renderer);
 		}
-
-		// Destroy window
-		SDL_DestroyWindow(window);
 	}
+
+	// Clean up audio
+	Mix_FreeChunk(waves);
+	//Mix_FreeMusic(clapnsnare1);
+	Mix_FreeMusic(clapnsnare2);
+	//Mix_FreeMusic(clapnsnare3);
+	//Mix_FreeMusic(clapnsnare4);
 
 	// Quit SDL2_mixer
 	Mix_CloseAudio();
