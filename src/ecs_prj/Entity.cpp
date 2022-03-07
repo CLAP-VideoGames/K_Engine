5f8f7555e711f98f0aed6bfdbd3b2056db8a347a
Ángel López <angelo06@ucm.es>
2022-03-07 12:11:53 +0100

    GetComponent, HasComponent  y RemoveComponent con templates e ids

--- src/ecs_prj/Entity.cpp
+++ src/ecs_prj/Entity.cpp
@@ -1,8 +1,10 @@
 #include "Entity.h"
+#include <stdexcept>
 
 Entity::Entity()
 {
 	active = true;
+	toDestroy = false;
 }
 
 Entity::~Entity()
@@ -10,6 +12,64 @@ Entity::~Entity()
 
 }
 
-Component* Entity:: getComponent(std::string id_) {
-	
+template<typename T>
+Component* Entity::getComponent()
+{
+	std::string compName;
+	try {
+
+		compName = T::getId();
+	}
+	catch(std::string msg) {
+		throw std::invalid_argument("There is no component with that name");
+	}
+
+	auto iterator = components.find(compName);
+		
+	if (iterator != components.end()) return components[compName];
+
+	return nullptr;															
+}
+
+template<typename T>
+void Entity::removeComponent()
+{
+	std::string compName;
+	try {
+		compName = T::getId();
+	}
+	catch(std::string msg) {
+		throw std::invalid_argument("There is no component with that name");
+	}
+
+	auto iterator = components.find(compName);
+
+	if (hasComponent<T>()) {		
+		auto it = components.find(compName);
+
+		components.erase(it);
+	}
+
+	delete components[compName];
+
+	components[compName] = nullptr;
+}
+
+template<typename T>
+void Entity::hasComponent()
+{
+	std::string compName;
+
+	try {
+		compName = T::getId();
+	}
+	catch(std::string msg) {
+		throw std::invalid_argument("There is no component with that name");
+	}
+
+	auto iterator = components.find(compName);
+
+	if (iterator != components.end()) return true;
+
+	return false;
 }
--- src/ecs_prj/Entity.h
+++ src/ecs_prj/Entity.h
@@ -9,20 +9,29 @@ public:
 	Entity();
 	~Entity();
 
-	Component* getComponent(std::string id_);
+	template<typename T>
+	Component* getComponent();
 
-	void setActive(bool a) { active = false; }
+	template<typename T>
+	void removeComponent();
+
+	template <typename T>
+	void hasComponent();
+
+	void setActive(bool a) { active = a; }
 private:
 
 	//bool to determine if we update the entity or not
 	bool active;
+	//bool to know when do we have to erase an entity
+	bool toDestroy;
 
 	//Map to find our components and erase them using the id of the component
-	std::unordered_map<std::string, Component>components;
-
-	//Map to find our components by number
-	std::unordered_map<int, Component>components;
-
+	std::unordered_map<std::string, Component*>components;
+		
+	//strings to give the entity a tag or a name
+	std::string tag;
+	std::string name;
 
 	//addComponent(Component c){
 	//}
