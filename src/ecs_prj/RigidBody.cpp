c14037e564fdfe23eb030aedc045e3831776b942
Aerdis <omaya01@ucm.es>
2022-03-09 23:23:25 +0100

    RigidBody basico terminado

---
+++ src/ecs_prj/RigidBody.cpp
@@ -0,0 +1,11 @@
+#include "RigidBody.h"
+//Required
+std::string RigidBody::name = "RigidBody";
+
+RigidBody::RigidBody() : Component("RigidBody") {
+
+}
+
+RigidBody::~RigidBody() {
+	
+}
--- src/ecs_prj/RigidBody.h
+++ src/ecs_prj/RigidBody.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "Component.h"
-#include <vector>
+
+class btCollisionShape;
+class btRigidBody;
+class btDefaultMotionState;
+
+namespace BtOgre {
+
+}
 
 class RigidBody : public Component
 {
@@ -15,12 +22,24 @@ protected:
 
 
 private:
+	//Boolean to control if the associated trigger is a trigger or not (default is false)
+	bool isTrigger = false;
+
+	//Reference to bullet rigidbody
+	btRigidBody* bulletRigidBody = nullptr;
+	
+	//Reference to the collision shape of this rigidbody
+	btCollisionShape* bulletCollisionShape = nullptr;
+
+	//Reference to the default motion state of this rigidbody
+	btDefaultMotionState* bulletDefMotionState = nullptr;
 
 	//Required
 	static std::string name;
 
-	//
+	//Array of constrains for rotation in x y z
 	bool rotationConstraints[3]{ false, false, false };
-	//
+
+	//Array of constrains for traslation in x y z
 	bool traslationConstraints[3]{ false, false, false };
 };
