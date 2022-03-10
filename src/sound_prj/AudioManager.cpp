50d4b2ae75feaf38471f2ba556cc085cb09d5cd7
David Brey <dbrey@ucm.es>
2022-03-11 00:07:08 +0100

    Añadidos metodos AudioManager

--- src/sound_prj/AudioManager.cpp
+++ src/sound_prj/AudioManager.cpp
@@ -45,9 +45,8 @@ bool AudioManager::Shutdown()
 	return true;
 }
 
-/// <summary>
-/// Load and Play audio sources
-/// </summary>
+//-------------------------------------------------------------------------------
+
 void AudioManager::loadWAV(const char* path)
 {
 	wav = Mix_LoadWAV(path);
@@ -64,19 +63,151 @@ void AudioManager::loadMUS(const char* path)
 		std::cout << "MUS sound could not be loaded \n";
 }
 
-void AudioManager::playWAV(const char* path)
-{
-	loadWAV(path);
+//-------------------------------------------------------------------------------
 
-	if (Mix_PlayChannel(-1, wav, 0) == -1)
+void AudioManager::playWAV(const char* path, int loop)
+{
+	// Load and set volume
+	loadWAV(path);
+	Mix_VolumeChunk(wav, volumeWAV); 
+
+	if (Mix_PlayChannel(-1, wav, loop) == -1)
 		std::cout << "WAV sound could not be played!\n";
 }
 
-void AudioManager::playMUS(const char* path)
+void AudioManager::playMUS(const char* path, int loop)
 {
 	loadMUS(path);
 
-	if (Mix_PlayMusic(mus, 0) == -1)
+	if (Mix_PlayMusic(mus, loop) == -1)
 		std::cout << "MUS sound could not be played!\n";
 }
 
+//-------------------------------------------------------------------------------
+
+void AudioManager::pauseMUS()
+{
+	if (mus != nullptr)
+	{
+		//if(Mix_PlayingMusic())
+			Mix_PauseMusic();
+	}
+	else
+	{
+		std::cout << "There is no MUS to pause\n";
+	}
+}
+
+void AudioManager::pauseWAV(int channel)
+{
+	// If channel = -1, then all channels will be paused
+	// If channel = -1, then all channels will be resumed
+	// If channel exists and it's not paused, then it will be resumed
+	if (channel == -1 || (Mix_GetChunk(channel) != nullptr && !Mix_Paused(channel)))
+	{
+		Mix_Pause(channel);
+	}
+	else
+	{
+		std::cout << "There is no WAV to pause\n";
+	}
+}
+
+//-------------------------------------------------------------------------------
+
+void AudioManager::resumeMUS()
+{
+	if (mus != nullptr)
+	{
+		Mix_ResumeMusic(); // This is safe to use on halted, paused, and already playing music.
+	}
+	else
+	{
+		std::cout << "There is no MUS to resume\n";
+	}
+}
+
+void AudioManager::resumeWAV(int channel)
+{
+	// Resume an especific WAV if it's paused
+	// If channel = -1, then all channels will be resumed
+	// If channel exists and it's paused, then it will be resumed
+	if (channel == -1 || (Mix_GetChunk(channel) != nullptr && Mix_Paused(channel)))
+	{
+		Mix_Resume(channel);
+	}
+	else 
+	{
+		std::cout << "There is no WAV to resume\n";
+	}
+}
+
+//-------------------------------------------------------------------------------
+
+void AudioManager::stopMUS()
+{
+	if (mus != nullptr)
+	{
+		Mix_HaltMusic();
+	}
+	else
+	{
+		std::cout << "There is no MUS to pause\n";
+	}
+}
+
+void AudioManager::stopWAV(int channel)
+{
+	// If channel = -1, then all channels will be stopped
+	// If the channel don't have a Mix_Chunk, then it's empty
+	if (channel == -1 || Mix_GetChunk(channel) != nullptr) 
+	{
+		Mix_HaltChannel(channel);
+	}
+	else
+	{
+		std::cout << "There is no WAV to pause\n";
+	}
+}
+
+void AudioManager::setVolumeMUS(int vol)
+{
+	volumeMUS = vol;
+
+	if (volumeMUS < 0)
+	{
+		volumeMUS = 0;
+	}
+	else if (volumeMUS > 128)
+	{
+		volumeMUS = 128;
+	}
+
+	Mix_VolumeMusic(vol);
+}
+
+void AudioManager::setVolumeWAV(int channel, int vol)
+{
+	volumeWAV = vol;
+	if (volumeWAV < 0)
+	{
+		volumeWAV = 0;
+	}
+	else if (volumeWAV > 128)
+	{
+		volumeWAV = 128;
+	}
+	Mix_Volume(channel, vol); // If channel = -1, all channel will have the same volume
+}
+
+//-------------------------------------------------------------------------------
+
+int AudioManager::getVolumeWAV()
+{
+	return volumeWAV;
+}
+
+int AudioManager::getVolumeMUS()
+{
+	return volumeMUS;
+}
