f55c3abee9957fb46fbd2b39cac6929ee3cfd2ac
mihern07 <mihern07@ucm.es>
2022-03-10 17:10:43 +0100

    Terminada clase contenedora de los metodos importantes de BtOgre

--- src/utils_prj/CustomBtOgre.h
+++ src/utils_prj/CustomBtOgre.h
@@ -86,12 +86,234 @@ namespace BtOgre {
 	class CollisionListener
 	{
 	public:
+		/// <summary>
+		/// Callback definition
+		/// </summary>
+		/// <param name="d"> btNearCallback used in bullet to determine collisions </param>
+		/// <param name="o"> Function to callback to </param>
 		CollisionListener(void(*d)(void*, void* other, const btManifoldPoint& mnf), void* o) : p(d), obj(o) {};
 		~CollisionListener() {};
 		void(*p)(void*, void* other, const btManifoldPoint& mnf);
 		void* obj;
 	};
 
+	/// <summary>
+	/// Contains all elements relevant to a physics object
+	/// </summary>
+	class RigidBody
+	{
+		btRigidBody* mBtBody;
+		btDynamicsWorld* mBtWorld;
+	public:
+		RigidBody(btRigidBody* btBody, btDynamicsWorld* btWorld) : mBtBody(btBody), mBtWorld(btWorld) {}
+		~RigidBody()
+		{
+			mBtWorld->removeRigidBody(mBtBody);
+			delete mBtBody->getUserPointer();
+			delete mBtBody->getMotionState();
+			delete mBtBody->getCollisionShape();
+			delete mBtBody;
+		}
+
+		btRigidBody* getBtBody() const { return mBtBody; }
+	};
+
+	/// <summary>
+	/// Contains all elements necessary for a Bullet physic world
+	/// </summary>
+	class DynamicsWorld
+	{
+		std::unique_ptr<btCollisionConfiguration> mCollisionConfig;
+		std::unique_ptr<btCollisionDispatcher> mDispatcher;
+		std::unique_ptr<btConstraintSolver> mSolver;
+		std::unique_ptr<btBroadphaseInterface> mBroadphase;
+		btDynamicsWorld* mBtWorld;
+	public:
+		explicit DynamicsWorld(const Ogre::Vector3& gravity);
+		~DynamicsWorld();
+		DynamicsWorld(btDynamicsWorld* btWorld) : mBtWorld(btWorld) {}
+
+		btRigidBody* addRigidBody(float mass, const Ogre::Entity* ent, ColliderType ct, void(*p)(void*, void* other, const btManifoldPoint& mnf), void* listener = 0);
+
+		btDynamicsWorld* getBtWorld() const { return mBtWorld; }
+	};
+
+	typedef std::vector<Ogre::Vector3> Vector3Array;
+	typedef std::map<unsigned char, Vector3Array*> BoneIndex;
+
+	/// <summary>
+	/// Converts an Ogre mesh to a physic object
+	/// </summary>
+	class VertexIndexToShape
+	{
+	public:
+		VertexIndexToShape(const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+		~VertexIndexToShape();
+
+		Ogre::Real getRadius();
+		Ogre::Vector3 getSize();
+
+
+		btSphereShape* createSphere();
+		btBoxShape* createBox();
+		btBvhTriangleMeshShape* createTrimesh();
+		btCylinderShape* createCylinder();
+		btConvexHullShape* createConvex();
+		btCapsuleShape* createCapsule();
+
+		const Ogre::Vector3* getVertices();
+		unsigned int getVertexCount();
+		const unsigned int* getIndices();
+		unsigned int getIndexCount();
+
+	protected:
+
+		void addStaticVertexData(const Ogre::VertexData* vertex_data);
+
+		void addAnimatedVertexData(const Ogre::VertexData* vertex_data,
+			const Ogre::VertexData* blended_data,
+			const Ogre::Mesh::IndexMap* indexMap);
+
+		void addIndexData(Ogre::IndexData* data, const unsigned int offset = 0);
+
+
+	protected:
+		Ogre::Vector3* mVertexBuffer;
+		unsigned int* mIndexBuffer;
+		unsigned int        mVertexCount;
+		unsigned int        mIndexCount;
+
+		Ogre::Vector3		mBounds;
+		Ogre::Real		    mBoundRadius;
+
+		BoneIndex* mBoneIndex;
+
+		Ogre::Matrix4		mTransform;
+
+		Ogre::Vector3		mScale;
+	};
+
+	/// <summary>
+	/// Static object converter
+	/// </summary>
+	class StaticMeshToShapeConverter : public VertexIndexToShape
+	{
+	public:
+
+		StaticMeshToShapeConverter(Ogre::Renderable* rend, const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+		StaticMeshToShapeConverter(const Ogre::Entity* entity, const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+		StaticMeshToShapeConverter();
+
+		~StaticMeshToShapeConverter() {};
+
+		void addEntity(const Ogre::Entity* entity, const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+
+		void addMesh(const Ogre::MeshPtr& mesh, const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+
+
+	protected:
+
+		const Ogre::Entity* mEntity;
+		Ogre::SceneNode* mNode;
+	};
+
+
+	/// <summary>
+	/// Converts a mesh or Ogre entity to a physic object. Compatible with ogre animations
+	/// </summary>
+	class AnimatedMeshToShapeConverter : public VertexIndexToShape
+	{
+	public:
+
+		AnimatedMeshToShapeConverter(Ogre::Entity* entity, const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+		AnimatedMeshToShapeConverter();
+		~AnimatedMeshToShapeConverter();
+
+		void addEntity(Ogre::Entity* entity, const Ogre::Matrix4& transform = Ogre::Matrix4::IDENTITY);
+		void addMesh(const Ogre::MeshPtr& mesh, const Ogre::Matrix4& transform);
+
+		btBoxShape* createAlignedBox(unsigned char bone,
+			const Ogre::Vector3& bonePosition,
+			const Ogre::Quaternion& boneOrientation);
+
+		btBoxShape* createOrientedBox(unsigned char bone,
+			const Ogre::Vector3& bonePosition,
+			const Ogre::Quaternion& boneOrientation);
+
+	protected:
+
+		bool getBoneVertices(unsigned char bone,
+			unsigned int& vertex_count,
+			Ogre::Vector3*& vertices,
+			const Ogre::Vector3& bonePosition);
+
+		bool getOrientedBox(unsigned char bone,
+			const Ogre::Vector3& bonePosition,
+			const Ogre::Quaternion& boneOrientation,
+			Ogre::Vector3& extents,
+			Ogre::Vector3* axis,
+			Ogre::Vector3& center);
+
+
+		Ogre::Entity* mEntity;
+		Ogre::SceneNode* mNode;
+
+		Ogre::Vector3* mTransformedVerticesTemp;
+		size_t               mTransformedVerticesTempSize;
+	};
+
+	/// <summary>
+	/// Redefines btIDebugDraw to render colliders in Ogre
+	/// </summary>
+	class DebugDrawer : public btIDebugDraw
+	{
+		Ogre::SceneNode* mNode;
+		btDynamicsWorld* mWorld;
+
+		Ogre::ManualObject mLines;
+		int mDebugMode;
+	public:
+		DebugDrawer(Ogre::SceneNode* node, btDynamicsWorld* world)
+			: mNode(node), mWorld(world), mLines(""), mDebugMode(DBG_DrawWireframe)
+		{
+			mNode->attachObject(&mLines);
+			mWorld->setDebugDrawer(this);
+		}
+
+		void update()
+		{
+			mWorld->debugDrawWorld();
+			mLines.end();
+		}
+
+		void drawLine(const btVector3& from, const btVector3& to, const btVector3& color);
+
+		void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color)
+		{
+			drawLine(PointOnB, PointOnB + normalOnB * distance * 20, color);
+		}
+
+		void reportErrorWarning(const char* warningString)
+		{
+			Ogre::LogManager::getSingleton().logWarning(warningString);
+		}
+
+		void draw3dText(const btVector3& location, const char* textString)
+		{
+		}
+
+		void setDebugMode(int mode)
+		{
+			mDebugMode = mode;
+
+			if (mDebugMode == DBG_NoDebug)
+				clear();
+		}
+
+		void clear() { mLines.clear(); }
+
+		int getDebugMode() const { return mDebugMode; }
+	};
 }
 
 
