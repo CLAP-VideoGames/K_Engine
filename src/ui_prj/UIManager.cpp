5baa33cbcca80801654d7dd3faa1231411c3ce8b
Ángel López <angelo06@ucm.es>
2022-03-23 23:04:13 +0100

    Vuelve a ejecutar pero flickerea otra vez

--- src/ui_prj/UIManager.cpp
+++ src/ui_prj/UIManager.cpp
@@ -34,8 +34,6 @@ namespace K_Engine {
 
             instance.get()->initContext();
             instance.get()->initRoot();
-
-            CEGUI::WindowManager::setDefaultResourceGroup("Layouts");
         }
         catch (const std::exception&) {
             return false;
@@ -90,78 +88,76 @@ namespace K_Engine {
     {
 
        SchemeManager::getSingleton().createFromFile("TaharezLook.scheme");
-       /* CEGUI::DefaultResourceProvider* rp = static_cast<CEGUI::DefaultResourceProvider*>
-            (CEGUI::System::getSingleton().getResourceProvider());
 
-        rp->setResourceGroupDirectory("layouts", "Layouts");*/
+       // CEGUI relies on various systems being set-up, so this is what we do
+       // here first.
+       //
+       // The first thing to do is load a CEGUI 'scheme' this is basically a file
+       // that groups all the required resources and definitions for a particular
+       // skin so they can be loaded / initialised easily
+       //
+       // So, we use the SchemeManager singleton to load in a scheme that loads the
+       // imagery and registers widgets for the TaharezLook skin.  This scheme also
+       // loads in a font that gets used as the system default.
+       //CEGUI::Window* wnd =  winMgr->loadLayoutFromFile("K_EngineImage.layout");
 
-        //CEGUI::WindowManager::setDefaultResourceGroup("Layouts");
+       FrameWindow* wnd = (FrameWindow*)winMgr->createWindow("TaharezLook/Label", "Sample Window");
+ 
 
-        // CEGUI relies on various systems being set-up, so this is what we do
-        // here first.
-        //
-        // The first thing to do is load a CEGUI 'scheme' this is basically a file
-        // that groups all the required resources and definitions for a particular
-        // skin so they can be loaded / initialised easily
-        //
-        // So, we use the SchemeManager singleton to load in a scheme that loads the
-        // imagery and registers widgets for the TaharezLook skin.  This scheme also
-        // loads in a font that gets used as the system default.
-        CEGUI::Window* wnd =  winMgr->loadLayoutFromFile("TaharezLookOverview.layout");
+       // The next thing we do is to set a default cursor image.  This is
+       // not strictly essential, although it is nice to always have a visible
+       // indicator if a window or widget does not explicitly set one of its own.
+       //
+       // The TaharezLook Imageset contains an Image named "MouseArrow" which is
+       // the ideal thing to have as a defult cursor image.
+       //guiContext->getMouseCursor().setDefaultImage("TaharezLook/MouseArrow");
 
-        // The next thing we do is to set a default cursor image.  This is
-        // not strictly essential, although it is nice to always have a visible
-        // indicator if a window or widget does not explicitly set one of its own.
-        //
-        // The TaharezLook Imageset contains an Image named "MouseArrow" which is
-        // the ideal thing to have as a defult cursor image.
-        //guiContext->getMouseCursor().setDefaultImage("TaharezLook/MouseArrow");
+       // load font and setup default if not loaded via scheme
+       //Font& defaultFont = FontManager::getSingleton().createFromFile("DejaVuSans-12.font");
+       // Set default font for the gui context
+       //guiContext->setDefaultFont(&defaultFont);
 
-        // load font and setup default if not loaded via scheme
-        //Font& defaultFont = FontManager::getSingleton().createFromFile("DejaVuSans-12.font");
-        // Set default font for the gui context
-        //guiContext->setDefaultFont(&defaultFont);
+       // Set the root window as root of our GUI Context
+       guiContext->setRootWindow(mRoot);
 
-        // Set the root window as root of our GUI Context
-        guiContext->setRootWindow(mRoot);
+       // A FrameWindow is a window with a frame and a titlebar which may be moved around
+       // and resized.
+       //
+       // Create a FrameWindow in the TaharezLook style, and name it 'Sample Window'
+       //FrameWindow* wnd = (FrameWindow*)winMgr->createWindow("TaharezLook/FrameWindow", "Sample Window");
 
-        // A FrameWindow is a window with a frame and a titlebar which may be moved around
-        // and resized.
-        //
-        // Create a FrameWindow in the TaharezLook style, and name it 'Sample Window'
-        //FrameWindow* wnd = (FrameWindow*)winMgr->createWindow("TaharezLook/FrameWindow", "Sample Window");
+       // Here we attach the newly created FrameWindow to the previously created
+       // DefaultWindow which we will be using as the root of the displayed gui.
+       mRoot->addChild(wnd);
 
-        // Here we attach the newly created FrameWindow to the previously created
-        // DefaultWindow which we will be using as the root of the displayed gui.
-        mRoot->addChild(wnd);
+       // Windows are in Relative metrics mode by default.  This means that we can
+       // specify sizes and positions without having to know the exact pixel size
+       // of the elements in advance.  The relative metrics mode co-ordinates are
+       // relative to the parent of the window where the co-ordinates are being set.
+       // This means that if 0.5f is specified as the width for a window, that window
+       // will be half as its parent window.
+       //
+       // Here we set the FrameWindow so that it is half the size of the display,
+       // and centered within the display.
+       wnd->setPosition(UVector2(cegui_reldim(0.05f), cegui_reldim(0.05f)));
+       wnd->setSize(USize(cegui_reldim(0.25f), cegui_reldim(0.25f)));
 
-        // Windows are in Relative metrics mode by default.  This means that we can
-        // specify sizes and positions without having to know the exact pixel size
-        // of the elements in advance.  The relative metrics mode co-ordinates are
-        // relative to the parent of the window where the co-ordinates are being set.
-        // This means that if 0.5f is specified as the width for a window, that window
-        // will be half as its parent window.
-        //
-        // Here we set the FrameWindow so that it is half the size of the display,
-        // and centered within the display.
-        wnd->setPosition(UVector2(cegui_reldim(0.05f), cegui_reldim(0.05f)));
-        wnd->setSize(USize(cegui_reldim(0.25f), cegui_reldim(0.25f)));
-        // now we set the maximum and minum sizes for the new window.  These are
-        // specified using relative co-ordinates, but the important thing to note
-        // is that these settings are aways relative to the display rather than the
-        // parent window.
-        //
-        // here we set a maximum size for the FrameWindow which is equal to the size
-        // of the display, and a minimum size of one tenth of the display.
-        wnd->setMaxSize(USize(cegui_reldim(1.0f), cegui_reldim(1.0f)));
-        wnd->setMinSize(USize(cegui_reldim(0.1f), cegui_reldim(0.1f)));
+       // now we set the maximum and minum sizes for the new window.  These are
+       // specified using relative co-ordinates, but the important thing to note
+       // is that these settings are aways relative to the display rather than the
+       // parent window.
+       //
+       // here we set a maximum size for the FrameWindow which is equal to the size
+       // of the display, and a minimum size of one tenth of the display.
+       wnd->setMaxSize(USize(cegui_reldim(1.0f), cegui_reldim(1.0f)));
+       wnd->setMinSize(USize(cegui_reldim(0.1f), cegui_reldim(0.1f)));
 
-        // As a final step in the initialisation of our sample window, we set the window's
-        // text to "Hello World!", so that this text will appear as the caption in the
-        // FrameWindow's titlebar.
-        wnd->setText("K_EngineUI works!");
+       // As a final step in the initialisation of our sample window, we set the window's
+       // text to "Hello World!", so that this text will appear as the caption in the
+       // FrameWindow's titlebar.
+       wnd->setText("K_EngineUI works!");
 
-        wnd->subscribeEvent(CEGUI::Window::EventMouseClick, Event::Subscriber(&UIManager::handleHelloWorldClicked, this));
+       wnd->subscribeEvent(CEGUI::Window::EventMouseClick, Event::Subscriber(&UIManager::handleHelloWorldClicked, this));
     }
 
     /*************************************************************************
