85bdab779a971937745df0578b45579e2441d65b
Aerdis <omaya01@ucm.es>
2022-03-26 14:15:06 +0100

    Estructura de LogManager añadida

--- src/log_prj/LogManager.cpp
+++ src/log_prj/LogManager.cpp
@@ -1,15 +1,48 @@
 #include "LogManager.h"
 
-#include <OgreRoot.h>
-#include <OgreLog.h>
-#include <OgreLogManager.h>
+#include <iostream>
 
-std::unique_ptr<LogManager> LogManager::instance = nullptr;
+namespace K_Engine {
+	std::unique_ptr<LogManager> LogManager::instance = nullptr;
 
-LogManager::LogManager() = default;
+	LogManager::LogManager() = default;
 
-LogManager::~LogManager() = default;
+	LogManager::~LogManager() = default;
 
-LogManager* LogManager::GetInstance() {
-	return instance.get();
+	bool LogManager::Init() {
+		try {
+			instance.reset(new LogManager());
+		}
+		catch (const std::exception&) {
+			return false;
+		}
+
+		return true;
+	}
+
+	bool LogManager::Shutdown() {
+		instance.reset();
+		return true;
+	}
+
+	LogManager* LogManager::GetInstance() {
+		return instance.get();
+	}
+
+	void LogManager::addLog(std::string mess, logType ty) {
+		log l; l.message = mess; l.type = ty;
+		reg.push_back(l);
+	}
+
+	void LogManager::printLog() {
+		for (log l : reg) {
+			std::cout << l.type << " - " << l.message << "\n";
+		}
+
+		// it'd be cool to create a txt file with the whole log but my mister piskas pisko paskas told not to for the time being
+	}
+
+	void LogManager::clearLog() {
+		reg.clear();
+	}
 }
--- src/log_prj/LogManager.h
+++ src/log_prj/LogManager.h
@@ -4,21 +4,38 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 
-class LogManager {
-public:
-	LogManager();
-	~LogManager();
 
-	static LogManager* GetInstance();
+namespace K_Engine {
+	class LogManager {
+	public:
+		enum logType { info, error };
 
-	static bool Init(std::string n);
-	static bool Shutdown();
-private:
-	static std::unique_ptr<LogManager> instance;
+		LogManager();
+		~LogManager();
 
-	bool terminado = true;
-};
+		static LogManager* GetInstance();
 
+		static bool Init();
+		static bool Shutdown();
+
+		void addLog(std::string mess, logType ty);
+
+		void printLog();
+
+		void clearLog();
+
+	private:
+		static std::unique_ptr<LogManager> instance;
+
+		struct log {
+			std::string message;
+			logType type;
+		};
+
+		std::vector<log> reg;
+	};
+}
 
 #endif
